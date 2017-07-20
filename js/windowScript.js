6488ecc909d1c87a2007ec6c610b8f88dae01acd
LeQuan <lequan@Kingswood.(none)>
2017-07-19 20:09:05 -0400

    Final version of Simple.

--- js/windowScript.js
+++ js/windowScript.js
@@ -1,8 +1,13 @@
+/* Author: LeQuan Ly
+   Copyright: (C) 2017 codekngswd
+   
+   Window JavaScript file */
+
 var width = window.innerWidth;
 var height = window.innerHeight;
 
 if (width > 414 || height > 768) {
-	window.location = "alternate.html";	
+	window.location = "desktop.html";	
 } else {
 	window.location = "mobile.html";
 }
